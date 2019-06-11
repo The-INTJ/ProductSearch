26a878f73334390a521529d3ce2af08022240e3f
Drew Taylor <raylotdreygone@gmail.com>
2019-06-10 21:16:35 -0400

    Working on basics:

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-        
+        /* gets current working directory */
+        Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
 
+
+
+        File dir = new File(path.toString()); /* Creates file (a directory) from the above path name */
+
+        File[] directoryListing = dir.listFiles(); /* creates array of all files in above directory */
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                
+            }
+        } else {
+
+        }
 
     }
 
